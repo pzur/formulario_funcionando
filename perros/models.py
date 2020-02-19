c279c054b2bd738d28234a1c2c047250d83f16c2
unknown <paolo.minaya.c@gmail.com>
2020-02-19 13:19:35 -0500

    se agrego mas parte en el formulario

--- perros/models.py
+++ perros/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class Cliente(models.Model):
+    SEXO = (('V','Varon'),('M','Mujer'))
     nombre = models.CharField(max_length=200)
     apellido = models.CharField(max_length=200)
     telefono = models.CharField(max_length=15,null=True)
     direccion = models.CharField(max_length=15, null=True)
+    sexo = models.CharField(max_length=20,choices=SEXO,null=True)
+    distrito =models.CharField(max_length=200, null=True)
 
 
 
